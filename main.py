9a582275b64f6c46404555c7a77d93e9957fdf85
Vedansh-Agarwal-2007 <vedanshagarwaldpr1234@gmail.com>
2026-03-14 21:03:58 +0530

    Improving CORS

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ontrack-mu-ten.vercel.app",
+        "http://localhost:5173", # standard vite local port
+        "http://localhost:3000"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
